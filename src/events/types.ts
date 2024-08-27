327b5e45723c5e3b03906ed8b022358cefef1dfa
omer <omerme13@gmail.com>
2024-08-27 18:33:03 +0300

    fix(types): naming

--- src/events/types.ts
+++ src/events/types.ts
@@ -1,12 +1,12 @@
 export enum OrderStatus {
 	// Order has been created, but the ticket it is trying to order has not been reserved
-	Created = 'created',
+	Created = 'Created',
 	// The ticket the order is trying to reserve has already been reserved
 	// The user has cancelled the order
 	// The order has expired before payment
-	Cancelled = 'cancelled',
+	Cancelled = 'Cancelled',
 	// the order has successfully reserved the ticket
-	AwaitingPayment = "awaiting payment",
+	AwaitingPayment = "Awaiting Payment",
 	// The order has reserved the ticket and the user has provided payment successfully
 	Completed = 'Completed',
 }
